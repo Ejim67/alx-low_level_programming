f2065a7ecc281f7146fe6be63f93a2288b5c00b2
ejim67 <ejim67@gmail.com>
2023-07-16 14:22:17 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * main - displays the alphabet in lowercase
+ * using loop
+ * Return: Always 0.
  */
 int main(void)
 {
-int n, m;
+	char letter;
 
-for (n = 48; n <= 56; n++)
-{
-for (m = 49; m <= 57; m++)
-{
-if (m > n)
-{
-putchar(n);
-putchar(m);
-if (n != 56 || m != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
